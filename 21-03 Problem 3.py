1c2543a6b8831decfe32df2e67691da748129b2f
Aryanpatel034 <ap.aryanpatel1@gmail.com>
2025-03-21 10:25:58 +0530

    Delete 21-03 Problem 3.py

--- 21-03 Problem 3.py
+++
@@ -1,9 +0,0 @@
-def sum_avg(marks):
-    total = sum(marks)
-    average = total/len(marks)
-    return total,average
-
-marks=[96,95,71,64,84]
-total,average=sum_avg(marks)
-print(f"Total Marks:{total}")
-print(f"Average Marks:{average}")
